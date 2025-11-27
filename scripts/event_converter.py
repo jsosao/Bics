c1589f5a8a3c595e0b1aacaf7df5a1fc6d96ba3d
jsosao <79808095+jsosao@users.noreply.github.com>
2025-11-27 09:00:22 -0600

    Update event_converter.py

--- scripts/event_converter.py
+++ scripts/event_converter.py
@@ -12,8 +12,6 @@ from datetime import timedelta
 
 
 # Al inicio del script, crear una única timestamp para toda la ejecución
-# Formatear
-##EXECUTION_TIMESTAMP = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 EXECUTION_TIMESTAMP = (datetime.now() - timedelta(hours=6)).strftime('%Y-%m-%d %H:%M:%S')
 
 
