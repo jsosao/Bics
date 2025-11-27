31c801ca5f256a9422796a01b1f4a0172d369061
jsosao <79808095+jsosao@users.noreply.github.com>
2025-11-27 08:56:15 -0600

    Update event_converter.py

--- scripts/event_converter.py
+++ scripts/event_converter.py
@@ -11,7 +11,9 @@ from datetime import datetime
 
 # Al inicio del script, crear una única timestamp para toda la ejecución
 # Formatear
-#EXECUTION_TIMESTAMP = datetime.now().strftime('%Y-%m-%d %H:%M:%S') - 6
+##EXECUTION_TIMESTAMP = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+EXECUTION_TIMESTAMP = (datetime.now() - timedelta(hours=6)).strftime('%Y-%m-%d %H:%M:%S')
+
 
 # ============================================================
 # CONFIGURACIÓN DE URLs (desde variables de entorno)
