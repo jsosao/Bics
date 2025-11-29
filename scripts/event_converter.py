d9362d752a1496c0d376f9e05d32833da71d31e4
jsosao <79808095+jsosao@users.noreply.github.com>
2025-11-29 15:05:23 -0600

    Update event_converter.py

--- scripts/event_converter.py
+++ scripts/event_converter.py
@@ -320,8 +320,9 @@ CONVERTERS = {
 # ============================================================
 
 CUSTOM_FILTERS = {
-    'alfa_eventos': lambda group, title: any(x in group.lower() for x in ["futbol", "ppv", "lmb y lmp"]),
-    #'alfa_eventos': lambda group, title: "(eventos)" in group.lower() or "cielo sport" in title.lower() or "cielo evento" in title.lower(),    
+    #'alfa_eventos': lambda group, title: any(x in group.lower() for x in ["futbol", "ppv", "lmb y lmp"]),
+    'alfa_eventos': lambda group, title: "(eventos)" in group.lower(),    
+    #'alfa_eventos': lambda group, title: "(eventos)" in group.lower() or "cielo sport" in title.lower() or "cielo evento" in title.lower(),        
     #'pass_eventos': lambda group, title: any(x in group.lower() for x in ["nba", "nhl", "nfl", "mlb", "ncaaf"]),
     'pass_ncaaf': lambda group, title: "ncaaf" in group.lower(),  
     'pass_nba': lambda group, title: "nba" in group.lower(),    
